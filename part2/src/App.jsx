80c7298b9667c58f9c6a2ed317af3d59ef5c8d02
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-12-20 18:09:31 +0530

    Part 2 Exercise 2.6 phonebook step1

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,52 +1,42 @@
-import Course from "./components/Course.jsx";
-
-const courses = [
-  {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  },
-  {
-    name: "Node.js",
-    id: 2,
-    parts: [
-      {
-        name: "Routing",
-        exercises: 3,
-        id: 1,
-      },
-      {
-        name: "Middlewares",
-        exercises: 7,
-        id: 2,
-      },
-    ],
-  },
-];
+import { useState } from "react";
 
 const App = () => {
-  return <Course course={courses} />;
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+    setPersons([...persons, newPerson]);
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    const newName = event.target.value;
+    setNewName(newName);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name : <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addName}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{listStyle: 'none'}}>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
