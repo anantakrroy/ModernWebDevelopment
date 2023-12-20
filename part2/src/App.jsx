fc1eb65bd272e0aaccf53a0c9fd09b2fbf131e7b
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-12-20 22:06:55 +0530

    Part 2 Exercise 2.8 phonebook step3

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -3,21 +3,24 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   const isNamePresent = (name) => {
-    return persons.find(e => e.name.toLowerCase() === name.toLowerCase());
-  }
+    return persons.find((e) => e.name.toLowerCase() === name.toLowerCase());
+  };
 
-  const addName = (event) => {
+  const addEntry = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      phone: newPhone
     };
-    if(isNamePresent(newName)) {
+    if (isNamePresent(newName)) {
       alert(`${newName} is already added to the phonebook ! `);
     } else {
       setPersons([...persons, newPerson]);
       setNewName("");
+      setNewPhone("");
     }
   };
 
@@ -26,22 +29,32 @@ const App = () => {
     setNewName(newName);
   };
 
+  const handlePhoneChange = (event) => {
+    const phoneNumber = event.target.value;
+    setNewPhone(phoneNumber);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name : <input value={newName} onChange={handleNameChange} />
+          Name : <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={addName}>
+          Number : <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addEntry}>
             add
           </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul style={{listStyle: 'none'}}>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      <ul style={{ listStyle: "none" }}>
+        {persons.map((person) => (
+          <li key={person.name}><span style={{"padding": "15px"}}>{person.name} </span><span> {person.phone} </span></li>
+        ))}
       </ul>
     </div>
   );
