f03fc5de6804f2ae72782307f04471836046f1be
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-12-20 16:21:42 +0530

    Part 2b Forms

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,52 +1,45 @@
-import Course from "./components/Course.jsx";
+import { useState } from "react";
+import Note from "./components/Note";
 
-const courses = [
-  {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  },
-  {
-    name: "Node.js",
-    id: 2,
-    parts: [
-      {
-        name: "Routing",
-        exercises: 3,
-        id: 1,
-      },
-      {
-        name: "Middlewares",
-        exercises: 7,
-        id: 2,
-      },
-    ],
-  },
-];
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
-const App = () => {
-  return <Course course={courses} />;
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      id: notes.length + 1,
+      important: Math.random() < 0.5,
+    };
+    setNotes([...notes, noteObject]);
+    setNewNote("a new note...");
+  };
+
+  const handleInputChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>Show {showAll ? "Important" : "All"}</button>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleInputChange} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
