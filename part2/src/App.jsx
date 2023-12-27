9a0c8324a6cd56f50abe90b0f256498f40031c22
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-12-27 16:25:44 +0530

    Part 2 exercise 2.11 phonebook step6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Person from "./components/Person";
 import Form from "./components/Form";
 import Search from "./components/Search";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [nameFilter, setNameFilter] = useState("");
+
+  const hook = () => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => setPersons(response.data));
+  }
+  useEffect(hook, []);
 
   const isNamePresent = (name) => {
     return persons.find((e) => e.name.toLowerCase() === name.toLowerCase());
@@ -51,7 +54,6 @@ const App = () => {
 
   return (
     <div>
-      
       <h2>Phonebook</h2>
       <Search filterByName={filterNames} />
       <Form
