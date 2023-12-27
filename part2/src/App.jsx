81b947fc3badd2af748017aa0d4551e27c5c7c9d
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-12-27 14:33:45 +0530

    Part 2c - getting data from server

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }
+
+  useEffect(hook, []);
+  console.log("render", notes.length, "notes");
 
   const notesToShow = showAll
     ? notes
@@ -28,7 +40,9 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
-      <button onClick={() => setShowAll(!showAll)}>Show {showAll ? "Important" : "All"}</button>
+      <button onClick={() => setShowAll(!showAll)}>
+        Show {showAll ? "Important" : "All"}
+      </button>
       <ul>
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
