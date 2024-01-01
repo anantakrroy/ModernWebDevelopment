b7ec8ff36229e16e0b5ca34e2b129eb8890fbb9c
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-01-01 13:18:25 +0530

    lesson code part 2.8: add footer, error components

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,23 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
+import Footer from "./components/Footer"
 import noteService from "./services/notes";
 
-const Footer = () => {
-  const footerStyle = {
-    color: "green",
-    fontStyle: "italic",
-    fontSize: 16,
-  };
-  return (
-    <div style={footerStyle}>
-      <br />
-      <em>
-        Note app, Department of Computer Science, University of Helsinki 2023
-      </em>
-    </div>
-  );
-};
 
 const App = (props) => {
   const [notes, setNotes] = useState([]);
