933a29deef979ff00936cc3c663ed02fa5779ae5
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-01-12 18:00:26 +0530

    rectify getAll method

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -4,12 +4,7 @@ const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  const nonexistingNote = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    important: true,
-  }
-  return request.then((response) => response.data.concat(nonexistingNote));
+  return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
