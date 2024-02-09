02007b74f76c77e6528f8f9dc9cfe720c29884a7
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-02-09 10:09:34 +0530

    Refactor: Part1 : exercise 1.11 unicafe moved to subfolder

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+
+const StatisticLine = (props) => {
+  if (props.text === "Positive rating %") {
+    return (
+      <>
+        <tr>
+          <td style={{padding: "5px"}}>{props.text}</td>
+          <td style={{padding: "5px"}}>{props.value} %</td>
+        </tr>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <tr>
+          <td style={{padding: "5px"}}>{props.text}</td>
+          <td style={{padding: "5px"}}>{props.value}</td>
+        </tr>
+      </>
+    );
+  }
+};
+
+const Statistics = ({ good, bad, neutral, total, average, positive }) => {
+  if (good === 0 && bad === 0 && neutral === 0) return <p>No feedback given</p>;
+  else {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="All" value={total} />
+            <StatisticLine text="Average" value={average} />
+            <StatisticLine text="Positive rating %" value={positive} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  );
+};
+
+const App = () => {
+  // save feedback into its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const handleGood = () => {
+    const updateGood = good + 1;
+    const updateTotal = total + 1;
+    const updatePositive = Number((updateGood / updateTotal) * 100).toFixed(2);
+    const updateAverage = (updateGood - bad) / updateTotal;
+    setAverage(updateAverage);
+    setGood(updateGood);
+    setTotal(updateTotal);
+    setPositive(updatePositive);
+  };
+  const handleNeutral = () => {
+    const updateNeutral = neutral + 1;
+    const updateTotal = total + 1;
+    const updateAverage = (good - bad) / updateTotal;
+    const updatePositive = Number((good / updateTotal) * 100).toFixed(2);
+    setAverage(updateAverage);
+    setNeutral(updateNeutral);
+    setTotal(updateTotal);
+    setPositive(updatePositive);
+  };
+  const handleBad = () => {
+    const updateBad = bad + 1;
+    const updateTotal = total + 1;
+    const updateAverage = (good - updateBad) / updateTotal;
+    const updatePositive = Number((good / updateTotal) * 100).toFixed(2);
+    setAverage(updateAverage);
+    setBad(updateBad);
+    setTotal(updateTotal);
+    setPositive(updatePositive);
+  };
+
+  return (
+    <div>
+      <h1>Feedback</h1>
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleNeutral} text="Neutral" />
+      <Button onClick={handleBad} text="Bad" />
+      <h1>Statistics</h1>
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        total={total}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  );
+};
+
+export default App;
