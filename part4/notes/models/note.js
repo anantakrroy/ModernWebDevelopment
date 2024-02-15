33c63d5690518a6c1544fd43a91e645fb1df7e26
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-02-15 21:00:54 +0530

    Modified note model to move DB connection logic to app.js

--- part4/notes/models/note.js
+++ part4/notes/models/note.js
@@ -2,13 +2,6 @@ const mongoose =  require('mongoose')
 
 require('dotenv').config()
 
-const url = process.env.MONGO_URI
-console.log('Connecting to DB.... ',)
-
-mongoose.connect(url)
-  .then(result => console.log(`Connected to DB...${result}`))
-  .catch(error => console.log('Error connecting to DB....', error))
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
