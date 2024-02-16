39d8d1b51cb8f502b1a8ee1b94ec69e840493230
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-02-16 16:45:22 +0530

    Part 4: exercise 4.1 - Blog List , step 1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl =
+  "mongodb+srv://anantacodes:3jQBsNNrWJk83M2O@firstcluster.kbjdkfq.mongodb.net/bloglist?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log(`Connected to database.....`))
+  .catch((error) => console.log(`Error connecting to DB >>> ${error}`));
+  
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => response.json(blogs));
+});
+
+app.post("/api/blogs", (request, response) => {
+  const newBlog = new Blog(request.body);
+  newBlog.save().then((result) => response.status(201).json(result));
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port : ${PORT}`);
+});
