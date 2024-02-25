565ee7ca811d184a91ad5bae26d5464f68ebee25
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-02-25 12:33:32 +0530

    Refactored test to use beforeEach

--- part4/notes/tests/node_api.test.js
+++ part4/notes/tests/node_api.test.js
@@ -1,10 +1,30 @@
-const { describe, test, after } = require('node:test')
+const { describe, test, after , beforeEach} = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
 
 const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+beforeEach(async() => {
+  await Note.deleteMany({})
+  let noteObj = new Note(initialNotes[0])
+  await noteObj.save()
+  noteObj = new Note(initialNotes[1])
+  await noteObj.save()
+})
 
 describe('notes api testing', () => {
   test('notes are returned as json', async () => {
@@ -16,13 +36,13 @@ describe('notes api testing', () => {
 
   test('there are two notes', async() => {
     const response =  await api.get('/api/notes')
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, initialNotes.length)
   })
 
   test('the first note is about HTML' , async() => {
     const response = await api.get('/api/notes')
     const contents = response.body.map(e => e.content)
-    assert.strictEqual(contents.includes('HTML is easy !'), true)
+    assert(contents.includes('HTML is easy'))
   })
 
   after(async () => {
