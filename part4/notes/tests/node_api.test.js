11e61cf0f348624b29fda66eb170e8a204a94854
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-02-27 12:24:27 +0530

    Added new tests and imported helper

--- part4/notes/tests/node_api.test.js
+++ part4/notes/tests/node_api.test.js
@@ -1,22 +1,13 @@
-const { describe, test, after , beforeEach} = require('node:test')
+const { describe, test, after , beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Note = require('../models/note')
+const { initialNotes, nonExistingId, notesInDb } = require('./test_helper')
 
 const api = supertest(app)
 
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-]
 
 beforeEach(async() => {
   await Note.deleteMany({})
@@ -43,6 +34,36 @@ describe('notes api testing', () => {
     const response = await api.get('/api/notes')
     const contents = response.body.map(e => e.content)
     assert(contents.includes('HTML is easy'))
+  })
+
+  test('a valid note can be added', async() => {
+    const newNote = {
+      content: 'async/await simplifies making async calls',
+      important: true
+    }
+
+    await api.post('/api/notes')
+      .send(newNote)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/notes')
+    const contents = response.body.map(notes => notes.content)
+    assert.strictEqual(response.body.length, initialNotes.length + 1)
+    assert(contents.includes('async/await simplifies making async calls'))
+  })
+
+  test('note without content will not be added', async() => {
+    const invalidNote = {
+      important : false
+    }
+
+    await api.post('/api/notes')
+      .send(invalidNote)
+      .expect(400)
+
+    const response = await api.get('/api/notes')
+    assert.strictEqual(response.body.length, initialNotes.length)
   })
 
   after(async () => {
