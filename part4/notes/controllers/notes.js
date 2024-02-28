afec24083da057b5789db653698a92b128d7d9eb
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-02-28 16:10:12 +0530

    Using async/await and also included error handling

--- part4/notes/controllers/notes.js
+++ part4/notes/controllers/notes.js
@@ -6,28 +6,31 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) response.json(note)
-      else response.status(404).end()
-    })
-    .catch((error) => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if(note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/',async (request, response, next) => {
   const body = request.body
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.status(201).json(savedNote)
-    })
-    .catch((error) => next(error))
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch (error) {
+    next(error)
+  }
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
