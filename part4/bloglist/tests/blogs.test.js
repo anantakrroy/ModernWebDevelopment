417172c0aec15c106808bd33c21b74e7bef67b1b
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-03-05 11:50:29 +0530

    Part 4: exercise 4.13 Blog list expansions step 1

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -1,4 +1,4 @@
-const {test, after, beforeEach} = require('node:test')
+const {test, after, beforeEach, describe} = require('node:test')
 const assert = require('node:assert')
 const supertest = require('supertest')
 const mongoose = require('mongoose')
@@ -6,64 +6,83 @@ const Blog = require('./../models/Blog')
 const testUtils = require('./test_utils')
 
 const app = require('./../app')
-const { request } = require('node:http')
 
 const api = supertest(app)
 
-beforeEach(async() => {
-    await Blog.deleteMany({})
-    for(let blog of testUtils.testBlogsList) {
-        await new Blog(blog).save()
-    }
-})
 
-test('GET request to / returns all blogs', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, 2)
-})
+describe('Blog list test suite', () => {
+    beforeEach(async() => {
+        await Blog.deleteMany({})
+        for(let blog of testUtils.testBlogsList) {
+            await new Blog(blog).save()
+        }
+    })
 
-test('Each blog has id identifier and not _id', async() => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.every(blog => blog.id), true)
-    assert.strictEqual(response.body.every(blog => blog._id), false)
-})
+    describe('Fetch blogs', () => {
+        test('GET request to / returns all blogs', async () => {
+            const response = await api.get('/api/blogs')
+            assert.strictEqual(response.body.length, 2)
+        })
+        
+        test('Each blog has id identifier and not _id', async() => {
+            const response = await api.get('/api/blogs')
+            assert.strictEqual(response.body.every(blog => blog.id), true)
+            assert.strictEqual(response.body.every(blog => blog._id), false)
+        })  
+    })
 
-test('Creates a new blog successfully', async()=> {
-    const response = await api.post('/api/blogs')
-        .send(testUtils.newBlog)
-        .expect('Content-Type',/application\/json/)
-        .expect(201)
-    const getResponse =  await api.get('/api/blogs')
-    const blogsLength = getResponse.body.length
-    assert.strictEqual(blogsLength, 3)
-    const findSavedBlog = await Blog.findOne({title : 'Exploring the native NodeJS test runner'})
-    assert(findSavedBlog)
-})
+    describe('Create a blog', () => {
+        test('Creates a new blog successfully', async()=> {
+            await api.post('/api/blogs')
+                .send(testUtils.newBlog)
+                .expect('Content-Type',/application\/json/)
+                .expect(201)
+            const getResponse =  await api.get('/api/blogs')
+            const blogsLength = getResponse.body.length
+            assert.strictEqual(blogsLength, 3)
+            const findSavedBlog = await Blog.findOne({title : 'Exploring the native NodeJS test runner'})
+            assert(findSavedBlog)
+        })  
 
-test('Default likes set to 0', async() => {
+        test('Missing title or url throws 400 status', async() => {
+            await api
+                .post('/api/blogs')
+                .send(testUtils.blogWithMissingTitle)
+                .expect(400)
+        
+            await api
+                .post('/api/blogs')
+                .send(testUtils.blogWithMissingUrl)
+                .expect(400)
+        
+            await api
+                .post('/api/blogs')
+                .send(testUtils.blogWithMissingTitleAndUrl)
+                .expect(400)
+        })    
+    })
+
+    describe('Default values set', () => {
+        test('Default likes set to 0', async() => {
     
-    const response = await api.post('/api/blogs')
-        .send(testUtils.blogWithMissingLikes)
-        .expect('Content-Type', /application\/json/)
-        .expect(201)
-    assert.strictEqual(response.body.likes,0)
-})
-
-test('Missing title or url throws 400 status', async() => {
-    await api
-        .post('/api/blogs')
-        .send(testUtils.blogWithMissingTitle)
-        .expect(400)
-
-    await api
-        .post('/api/blogs')
-        .send(testUtils.blogWithMissingUrl)
-        .expect(400)
-
-    await api
-        .post('/api/blogs')
-        .send(testUtils.blogWithMissingTitleAndUrl)
-        .expect(400)
+            const response = await api.post('/api/blogs')
+                .send(testUtils.blogWithMissingLikes)
+                .expect('Content-Type', /application\/json/)
+                .expect(201)
+            assert.strictEqual(response.body.likes,0)
+        })
+    })
+    
+    describe('Deletion of blogs', () => {
+        test('Blog with specific id is deleted', async () => {
+            const allBlogs = await Blog.find({})
+            const blogId = allBlogs[0].id
+            console.log(`Id to delete >>>>> ${typeof(blogId)}`)
+            await api
+                .delete(`/api/blogs/${blogId}`)
+                .expect(204)
+        })
+    })
 })
 
 after(async() => {
