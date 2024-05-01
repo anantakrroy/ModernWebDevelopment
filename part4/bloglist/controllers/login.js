380171f967752ea061e77e34cd629db4807e7f4e
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-05-01 14:02:40 +0530

    rectify: ppassword verification and refactor: error messages

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -8,22 +8,22 @@ loginRouter.post('/', async(request, response) => {
     const isUser = await User.findOne({username})
 
     if(!isUser) {
-        response.status(401).json({message : 'No user found !'})
+        response.status(401).json({error : `User "${username}" found !`})
+    } else {
+        const isPasswordCorrect = await bcrypt.compare(password, isUser.passwordHash)  
+        if(isPasswordCorrect){
+            const user = {
+                username: isUser.username,
+                id: isUser._id,
+            }
+            const token = jwt.sign(user, process.env.SECRET)
+            response.status(200).json({token, username: user.username, name: isUser.name})
+        } else {
+            response.status(401).json({error : 'Invalid password !'})
+        }
+         
     }
 
-    const isPasswordCorrect = bcrypt.compare(password, isUser.passwordHash)  
-    
-    if(isPasswordCorrect){
-        const user = {
-            username: isUser.username,
-            id: isUser._id,
-        }
-        const token = jwt.sign(user, process.env.SECRET)
-        response.status(200).json({token, username: user.username, name: isUser.name})
-    } else {
-        response.status(401).json({error : 'Invalid password !'})
-    }
-     
 })
 
 module.exports = loginRouter
