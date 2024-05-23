d88c07d36f186a861b0a482388266e915a6e7d2d
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-05-23 12:03:38 +0530

    Updated linting rules

--- part5/notes/.eslintrc.cjs
+++ part5/notes/.eslintrc.cjs
@@ -12,9 +12,14 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console' : 0
   },
 }
