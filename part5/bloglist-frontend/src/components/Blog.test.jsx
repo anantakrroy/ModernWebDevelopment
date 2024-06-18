254b1c689a13c8e0ceaf2c29923a3842f00074cd
Ananta Kr. Roy <kr.innovate@gmail.com>
2024-06-19 02:21:57 +0530

    Exercise 5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("Blog component renders author and title only", () => {
@@ -15,15 +16,49 @@ test("Blog component renders author and title only", () => {
     id: "6655d9fb3658db08ee7e4dc1",
   };
 
-//   render Blog component
-  const {container} = render(<Blog blog={blog} />)
-//   select the element
-  const div = container.querySelector(".blog")
-  screen.debug(div)
+  //   render Blog component
+  const { container } = render(<Blog blog={blog} />);
+  //   select the element
+  const div = container.querySelector(".blog");
+  //   screen.debug(div);
 
-  expect(div).toBeDefined()
-  expect(div).toHaveTextContent(blog.title)
-  expect(div).toHaveTextContent(blog.author)
-  expect(div).not.toHaveTextContent(blog.likes)
-  expect(div).not.toHaveTextContent(blog.url)
+  expect(div).toBeDefined();
+  expect(div).toHaveTextContent(blog.title);
+  expect(div).toHaveTextContent(blog.author);
+  expect(div).not.toHaveTextContent(blog.likes);
+  expect(div).not.toHaveTextContent(blog.url);
+});
+
+test("Blog component button click shows url and likes", async () => {
+  const blog = {
+    title: "Data types in Javascript",
+    author: "Lisa Tagliaferri",
+    url: "https://www.digitalocean.com/community/tutorials/understanding-data-types-in-javascript",
+    likes: 9,
+    user: {
+      username: "marypop",
+      name: "Mary Poppins",
+      id: "660cdabc0602286500d78c9e",
+    },
+    id: "6655d9fb3658db08ee7e4dc1",
+  };
+
+  //   render the Blog component
+  const { container } = render(<Blog blog={blog} />);
+  //   select the element with classname
+  const div = container.querySelector(".blog");
+  //   screen.debug(div)
+  //   select the button element
+  const button = screen.getByText(/View/);
+  //   create a mock event handler
+  const clickHandler = vi.fn();
+  //   screen.debug(button);
+  //   setup a new session
+  const user = userEvent.setup();
+  //   button click
+  await user.click(button);
+
+  //   assertions
+  expect(div).toHaveTextContent(`Likes : ${blog.likes}`);
+  expect(div).toHaveTextContent(blog.url);
 });
