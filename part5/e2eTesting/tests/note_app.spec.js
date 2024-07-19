a64f90d8980f2a4941fad622ede4c0a73554f531
anantakrroy <kr.innovate@gmail.com>
2024-07-19 14:50:45 +0530

    Added e2e tests for Login

--- part5/e2eTesting/tests/note_app.spec.js
+++ part5/e2eTesting/tests/note_app.spec.js
@@ -1,9 +1,38 @@
-const {test, expect} = require("@playwright/test")
+const { describe, beforeEach, test, expect } = require("@playwright/test");
 
-test('front page can be opened', async({page}) => {
+describe("Note App", () => {
+  beforeEach(async({page}) => {
     await page.goto('http://localhost:5173')
-    
-    const locator = await page.getByText('Notes')
-    await expect(locator).toBeVisible()
-    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
-})
+  })
+
+  test("front page can be opened", async ({ page }) => {
+    const locator = await page.getByText("Notes");
+    await expect(locator).toBeVisible();
+    await expect(
+      page.getByText(
+        "Note app, Department of Computer Science, University of Helsinki 2024"
+      )
+    ).toBeVisible();
+  });
+
+  test('Login form can be opened', async({page}) => {
+    await page.getByRole('button', /login/i).click()
+
+    // Use the first() & last() methods to find textboxes in login form -- not recommended
+    // await page.getByRole('textbox').first().fill('rootyroot')
+    // await page.getByRole('textbox').last().fill('root123')
+
+    // Use the all() method to get all the textboxes in the page -- not recommended
+    // const textboxes = await page.getByRole('textbox').all()
+    // await textboxes[0].fill('rootyroot')
+    // await textboxes[1].fill('root123')
+
+    // Use the getByTestId to get all the textboxes 
+    await page.getByTestId('username').fill('rootyroot')
+    await page.getByTestId('password').fill('root123')
+
+    await page.getByRole('button', {name : 'Login'}).click()
+
+    await expect(page.getByText('Bohe Root logged in')).toBeVisible()
+  })
+});
--- part5/notes/src/components/LoginForm.jsx
+++ part5/notes/src/components/LoginForm.jsx
@@ -12,6 +12,7 @@ const LoginForm = ({
       <div>
         username :
         <input
+          data-testid="username"
           type="text"
           value={username}
           name="Username"
@@ -21,6 +22,7 @@ const LoginForm = ({
       <div>
         password :
         <input
+          data-testid="password"
           type="password"
           value={password}
           name="Password"
