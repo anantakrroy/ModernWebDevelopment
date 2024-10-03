229fdedb7543b4fd5569d693cc6008131649c72e
anantakrroy <kr.innovate@gmail.com>
2024-10-03 12:09:47 +0530

    6.2: Unicafe Revisited, step 2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,36 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from 'redux';
+import reducer from './reducer';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
+  // handle button clicks - dispatch actions to store
+  const handleGood = () => {
     store.dispatch({
-      type: 'GOOD'
-    })
-  }
+      type: 'GOOD',
+    });
+  };
+
+  const handleOk = () => {
+    store.dispatch({ type: 'OK' });
+  };
+
+  const handleBad = () => {
+    store.dispatch({ type: 'BAD' });
+  };
+
+  const handleReset = () => {
+    store.dispatch({ type: 'ZERO' });
+  };
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleOk}>ok</button>
+      <button onClick={handleBad}>bad</button>
+      <button onClick={handleReset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
